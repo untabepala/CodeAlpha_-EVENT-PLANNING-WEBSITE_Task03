0dbdcc103a9611ccdbe8507d2898967992625134
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-02-10 08:37:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,27 @@ menu.onclick=() =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+/*let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('toggle-btn');
+
+toggleBtn.onclick =() =>{
+  themeToggler.classList.toggle('active');
+}*/
+
 window.onscroll=() =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+    /*themeToggler.classList.remove('active');*/
 }
+
+/*document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn =>{
+btn.onclick =() =>{
+  let color = btn.style.background;
+  document.querySelector('.root').style.setProperty('--main-color',color);
+}
+});*/
+
 var swiper = new Swiper(".home-slider", {
     effect: "coverflow",
     grabCursor: true,
@@ -28,5 +45,27 @@ var swiper = new Swiper(".home-slider", {
     autoplay:{
         delay:3000,
         disableOnInteraction:false,
+    }
+  });
+
+
+  var swiper = new Swiper(".review-slider", {
+    grabCursor: true,
+    loop: true,
+    spaceBetween: 10,
+    breakpoints: {
+      0:{
+        slidesPerView:1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1050: {
+        slidesPerView: 3,
+      },
+    },
+    autoplay:{
+      delay:5000,
+      disableOnInteraction:false,
     }
   });
